152d1a64500651425be3d1292121e547e0830861
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2024-02-24 02:27:25 +0300

    dto for services

--- gigapoll/dto.py
+++ gigapoll/dto.py
@@ -1,8 +1,31 @@
 from typing import NamedTuple
 
 
-class NewTemplateSettings(NamedTuple):
+class ButtonDTO(NamedTuple):
     description: str
     name: str
     button_name: str
     button_cbdata: str
+    votes: int
+
+    @property
+    def public_name(self) -> str:
+        return f'{self.button_name} ({self.votes} votes)'
+
+
+class CntPerValue(NamedTuple):
+    button_name: str
+    button_cbdata: str
+    cnt: int
+
+
+class UserChoiceDTO(NamedTuple):
+    first_name: str
+    last_name: str | None
+    username: str | None
+    choice: str
+
+
+class AggUsersByButton(NamedTuple):
+    button_value: str
+    users: list[str]
