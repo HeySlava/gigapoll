f24f3e7fd4a913f53dffbf6707703da6be5e71f0
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2024-02-24 16:54:08 +0300

    Add inline_user_html property

--- gigapoll/dto.py
+++ gigapoll/dto.py
@@ -20,12 +20,25 @@ class CntPerValue(NamedTuple):
 
 
 class UserChoiceDTO(NamedTuple):
+    user_id: int
     first_name: str
     last_name: str | None
     username: str | None
     choice: str
 
+    @property
+    def full_name(self) -> str:
+        return (
+                f'{self.first_name} {self.last_name}'
+                if self.last_name
+                else self.first_name
+            )
+
+    @property
+    def inline_user_html(self) -> str:
+        return f'<a href="tg://user?id={self.user_id}">{self.full_name}</a>'
+
 
 class AggUsersByButton(NamedTuple):
     button_value: str
-    users: list[str]
+    users: list[UserChoiceDTO]
