9272572ad474d87382aa739442bc18757684a00a
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2026-06-08 09:15:25 +0300

    fix: revert custom AiohttpSession timeout (incompatible with aiogram 3.3.0)

--- gigapoll/bot.py
+++ gigapoll/bot.py
@@ -1,11 +1,8 @@
 import os
 
-import aiohttp
 from aiogram import Bot
-from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
 
 TOKEN = os.environ['POLL_TOKEN']
 
-session = AiohttpSession(timeout=aiohttp.ClientTimeout(total=60))
-bot = Bot(TOKEN, parse_mode=ParseMode.HTML, session=session)
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
